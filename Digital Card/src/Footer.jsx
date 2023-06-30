afc46ad1c416e4dc52711b893974e2bb301b8b4a
Steve <scampbellatc@gmail.com>
2023-06-30 13:47:27 -0400

    added Vite

---
+++ Digital Card/src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <div>
+            <p>Stephen Campbell</p>
+            <p>Stephen Campbell</p>
+            <p>Stephen Campbell</p>
+            <p>Stephen Campbell</p>
+        </div>
+
+    )
+}
+
+export default Footer
