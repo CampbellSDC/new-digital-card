ba2b6d1969ec0399295bcf3665da30134bd6c375
Steve <scampbellatc@gmail.com>
2023-07-03 13:25:53 -0400

    updated styling, added icons

--- Digital Card/src/Content.jsx
+++ Digital Card/src/Content.jsx
@@ -6,12 +6,15 @@ function Content(){
             <div className="about-me">
                 <h3 className="about-me-title">About Me</h3>
 
-                <p className="about-me-text">Why I enjoy frontend development</p>
+                <p className="about-me-text">I am a frontend developer who also has experience working with backend (Node, Express and MongoDB). 
+                I enjoy the creative process, as well as providing unique solutions using best practices within the tech industry. I am always 
+                looking for new and interesting things to learn to further my skillset.</p>
             </div>
 
             <div className="interests-container">
                 <h3 className="interests-title">Interests</h3>
-                <p className="interests-text">Some other personal information about me</p>
+                <p className="interests-text">As former healthcare worker, I am interested in all things science. I am also a self-taught woodworker, creating custom 
+                furniture and other fine woodoworking pieces. Music, guitar, piano...and finding the best patio spots around town.</p>
             </div>
 
         </div>
